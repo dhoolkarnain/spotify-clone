99eff398ad81c954aa1b6fb8054b29cd95cf3aeb
Sufyan Ahmad Ansari <dhoolkarnain@gmail.com>
2022-07-08 22:01:29 +0530

    Updates

---
+++ precache-manifest.9030ab3b1e2cf70ee2e0afd7d95033d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e57c0a5e0b67bdd8ca0",
+    "url": "/spotify-clone/static/css/main.b60d69b2.chunk.css"
+  },
+  {
+    "revision": "7e57c0a5e0b67bdd8ca0",
+    "url": "/spotify-clone/static/js/main.7e57c0a5.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/spotify-clone/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "92b33a51a57dae606bf6",
+    "url": "/spotify-clone/static/js/2.92b33a51.chunk.js"
+  },
+  {
+    "revision": "5c939932f5ce74739841",
+    "url": "/spotify-clone/static/js/runtime~main.5c939932.js"
+  },
+  {
+    "revision": "d140e1538eaff433cb5fc95549fe0d17",
+    "url": "/spotify-clone/static/media/profile.d140e153.jpg"
+  },
+  {
+    "revision": "d37d2ec4e55ef4635bc65fb173a07032",
+    "url": "/spotify-clone/static/media/artist.d37d2ec4.jpg"
+  },
+  {
+    "revision": "73027150901a5666ed367e1d273ae650",
+    "url": "/spotify-clone/static/media/track.73027150.png"
+  },
+  {
+    "revision": "91fc254909fd46959222805c11d8452e",
+    "url": "/spotify-clone/static/media/background.91fc2549.jpg"
+  },
+  {
+    "revision": "17748dc791cfe0e50f68c966b8d0b73c",
+    "url": "/spotify-clone/index.html"
+  }
+];
